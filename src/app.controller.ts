85864eff4a240509b9600b422738c0ee70914196
Sam VanFossen <samvanf@gmail.com>
2022-10-11 17:32:44 -0400

    Adding linter Adding skeleton of post games route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,80 +1,76 @@
-import { Controller, Get, Patch, Query } from '@nestjs/common';
-import { ApiQuery } from '@nestjs/swagger';
+import { Controller, Get, Patch, Post, Query } from '@nestjs/common';
+import { ApiBody, ApiParam, ApiProperty, ApiQuery } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {
-
     this.min = 1;
 
-    this.max = 10;}
+    this.max = 10;
+  }
 
   private min: number;
 
-    private max: number;
+  private max: number;
 
- 
+  @Get('goal')
+  @ApiQuery({
+    type: 'number',
+    name: 'min',
+    description: 'lower bound for goal'
+  })
+  @ApiQuery({
+    type: 'number',
+    name: 'max',
+    description: 'upper bound for goal'
+  })
+  getRandom(@Query() query: { min: number; max: number }) {
+    return Math.ceil((query.max - query.min + 1) * Math.random() - 1) + query.min;
+  }
 
-    @Get('goal')
+  @Get('bounds')
+  getBounds() {
+    return {
+      min: this.min,
+      max: this.max,
+    };
+  }
 
-    getGoal() {
+  @Patch('bounds')
+  @ApiQuery({
+    type: 'number',
+    name: 'min',
+    description: 'min number in interval'
+  })
+  @ApiQuery({
+    type: 'number',
+    name: 'max',
+    description: 'max number in interval'
+  })
+  setBounds(@Query() query: { min: number; max: number }) {
+    this.min = query.min;
+    this.max = query.max;
+    return {
+      min: this.min,
+      max: this.max,
+    };
+  }
 
-        return Math.ceil((this.max - this.min + 1)*Math.random() -1) + this.min;
-
-    }
-
- 
-
-    @Get('bounds')
-
-    getBounds() {
-
-        return {
-
-            min: this.min,
-
-            max: this.max
-
-        }
-
-    } 
-
- 
-
-    @Patch('bounds')
-
-    @ApiQuery({
-
-        type: 'number',
-
-        name: 'max'
-
-    })
-
-    @ApiQuery({
-
-        type: 'number',
-
-        name: 'min'
-
-    })
-
-    setBounds(@Query() query: {min: number, max: number}) {
-
-        console.log(query)
-
-        this.min = query.min;
-
-        this.max = query.max;
-
-        return {
-
-            min: this.min,
-
-            max: this.max
-
-        }
-
-    }
+  @Post('games')
+  @ApiParam({
+    name: 'bounds',
+    required: false,
+    type: 'object'
+  })
+  // @ApiBody({
+  //   schema: {},
+  //   type: 'object'
+  // })
+  @ApiProperty({
+    description: 'Create a new game'
+  })
+  postGame() {
+    
+  }
 }
